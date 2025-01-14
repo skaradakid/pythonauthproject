200e48ab9359a49575073fdeb6a61ba59461e09c
kaphashjhb024 <kaphashjhb024@student.wethinkcode.co.za>
2025-01-14 15:36:57 +0200

    rigistered app

---
+++ main.py
@@ -0,0 +1,9 @@
+import pyrebase
+
+firebaseconfig = {apiKey: "AIzaSyCupc_lHjT_WtY9fC7yuuRDYTy7VuIRZn4",
+                authDomain: "pythonauthproject-ed1fe.firebaseapp.com",
+                projectId: "pythonauthproject-ed1fe",
+                storageBucket: "pythonauthproject-ed1fe.firebasestorage.app",
+                messagingSenderId: "895676141316",
+                appId: "1:895676141316:web:b7e3aaf5b99ada18fe4a51",
+                measurementId: "G-7CF4MK41HP"}
